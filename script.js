9fa0c238e82eefd0eeef7598789e53e2f1c2114f
Lisa Miner <midnightmoet@gmail.com>
2023-08-27 15:56:00 -0400

    Pushed through and finished this

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-const monthEl = document.querySelector(".date h1");
-const fullDateEl = document.querySelector(".date p");
+const monthElement = document.querySelector(".date h1");
+const fullDateElement = document.querySelector(".date p");
+const daysElement = document.querySelector(".days");
+
 const monthIndex = new Date().getMonth();
 const lastDay = new Date(new Date().getFullYear(), monthIndex + 1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthIndex, 1).getDay() - 1;
 
 
 const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
-monthEl.innerText = months[monthIndex];
-fullDateEl.innerText = new Date().toDateString();
+monthElement.innerText = months[monthIndex];
+fullDateElement.innerText = new Date().toDateString();
 
 let days = "";
 
+for (let i = firstDay; i > 0; i--) {
+  days += `<div class="empty"></div>`;
+}
 for (let i = 1; i <= lastDay; i++) {
+  if (i === new Date().getDate()) {
+    days += `<div class="today">${i}</div>`;
+  } else {
     days += `<div>${i}</div>`;
+  }
 }
 
+daysElement.innerHTML = days;
 
 // ----------------------------------------------
 // CONSOLE LOG TO TEST EVERYTHING IS WORKING
